b29636f3376e0722124733d3357ef5f5b69c20a5
Joe Dolson <design@joedolson.com>
2023-02-21 11:17:03 -0600

    Add PHPCS & docs build

---
+++ .github/hookdoc-tmpl/README.md
@@ -0,0 +1,5 @@
+## WP Accessibility Day Conference Plugin Hook Documentation
+
+This resource is generated documentation on actions and filters found in the WP Accessibility Day conference plugin. Use the sidebar to browse and navigate.
+
+To report an issue or contribute back to the project, please visit the [GitHub repository](https://github.com/WP-Accessibility-Day/conference).
---
+++ init.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link              https://wpconferenceschedule.com
+ * @since             1.0.0
+ * @package           wp_conference_schedule
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Accessibility Day - Conference Schedule
+ * Plugin URI:        https://wpconferenceschedule.com
+ * Description:       Forked from WP Conference Schedule by Road Warrior Creative.
+ * Version:           1.0.4
+ * Author:            WP Accessibility Day
+ * Author URI:        https://wpaccessibility.day
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-conference-schedule
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+include( dirname( __FILE__ ) . '/src/wp-conference-schedule.php' );
+
+/**
+ * Plugin Activation & Deactivation
+ */
+register_activation_hook( __FILE__, 'wpcsp_pro_activation');
+register_deactivation_hook( __FILE__, 'wpcsp_pro_deactivation');
+register_uninstall_hook( __FILE__, 'wpcsp_pro_uninstall');

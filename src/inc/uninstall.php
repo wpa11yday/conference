45a6055138d7b658a2eba607c9977ca1a5a97582
Nick Croft <NicktheGeek@NickGeek.com>
2023-02-22 16:27:45 -0500

    clean up src/inc/uninstall.php

--- src/inc/uninstall.php
+++ src/inc/uninstall.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * Uninstall functions.
+ *
+ * @package wpcsp
+ */
 
+/**
+ * Uninstall callback.
+ *
+ * @return void
+ */
 function wpcsp_pro_uninstall() {
-
 	delete_transient( 'wpcsp_license_valid' );
-
 }

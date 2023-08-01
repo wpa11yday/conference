03f9be3edc0c2ef0b4039f6bcb439283fb5c5c79
Amber Hinds <amberhinds@users.noreply.github.com>
2023-08-01 11:06:57 -0500

    Reverts the people and sessions post types to use the classic editor

--- src/inc/post-types.php
+++ src/inc/post-types.php
@@ -57,7 +57,7 @@ function wpcs_register_post_types() {
 			'hierarchical'       => false,
 			'query_var'          => true,
 			'menu_icon'          => 'dashicons-groups',
-			'show_in_rest'       => true,
+			'show_in_rest'       => false,
 			'rest_base'          => 'people',
 			'has_archive'        => false,
 		)
@@ -180,7 +180,7 @@ function wpcs_register_post_types() {
 			'hierarchical'    => false,
 			'query_var'       => true,
 			'menu_icon'       => 'dashicons-schedule',
-			'show_in_rest'    => true,
+			'show_in_rest'    => false,
 			'rest_base'       => 'sessions',
 		)
 	);

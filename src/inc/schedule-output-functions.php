223929d673f823a2239a184d7c57a7d7b51cbd03
Joe Dolson <design@joedolson.com>
2023-09-05 12:03:00 -0500

    Add filter to replace gravatar default with custom image

--- src/inc/schedule-output-functions.php
+++ src/inc/schedule-output-functions.php
@@ -119,6 +119,35 @@ function wpcs_shortcode_people( $atts ) {
 }
 
 /**
+ * Filter the default avatar fallback to use the WPAD logomark.
+ * 
+ * @param string     $avatar The HTML avatar returned by gravatar.
+ * @param string|int $id_or_email User info used to fetch gravatar.
+ * @param int        $size Size requested.
+ * @param string     $default Type of return. 
+ * @param string     $alt Expected alt text.
+ * @param array      $args Misc. extra arguments.
+ *
+ * @return string 
+ */
+function wpad_avatar( $avatar, $id_or_email, $size, $default, $alt, $args ) {
+	if ( is_admin() ) {
+		return $avatar;
+	}
+	// Set default response to 404, if no gravatar is found.
+	$avatar_url = str_replace( 'd=' . $args['default'], 'd=404', $args['url'] );
+	// Request the image url.
+	$response = wp_remote_head( $avatar_url );
+	// If there's no avatar, the default will be used, which results in 404 response
+	if ( 404 === wp_remote_retrieve_response_code( $response ) ) {
+		$avatar = '<img src="https://2023.wpaccessibility.day/wp-content/uploads/2023/08/cropped-cropped-WPAD_logomark-192x192.png" alt="" width="96" height="96">';
+	}
+	// Return img html
+	return $avatar;
+}
+add_filter( 'get_avatar', 'wpad_avatar', 10, 6 );
+
+/**
  * Get sessions scheduled for conference.
  *
  * @return array

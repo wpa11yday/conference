f88d3dbbc20199d4b3c0c10426ce555344e2357a
Joe Dolson <design@joedolson.com>
2025-05-04 14:39:08 -0500

    Misc. updates and code sniffs.

--- src/inc/social.php
+++ src/inc/social.php
@@ -5,35 +5,35 @@
  * @package wpcsp
  */
 
-/*
+/**
  * Get the post data that will be sent to social sharing pages.
- * 
+ *
  * @param integer $post_ID ID of the current post.
  *
  * @return array of post data for use in sharing.
  */
 function wpcsp_post_information( $post_ID ) {
-	$data          = array();
+	$data         = array();
 	$data['text'] = get_the_title( $post_ID ); 
-	$data['url']   = get_permalink( $post_ID );
+	$data['url']  = get_permalink( $post_ID );
 
 	return $data;
 }
 
-/* 
+/**
  * Generate the URLs used to post data to services.
- * 
+ *
  * @param integer $post_ID of current post
- * 
+ *
  * @return array of URLs for posting to each service.
  */
 function wpcsp_create_urls( $post_ID ) {
 	$data      = wpcsp_post_information( $post_ID );
-	$twitter   = "https://x.com/intent/tweet?text=" . urlencode( $data['title']  ). '&url=' . urlencode( $data['url'] );
-	$facebook  = "https://www.facebook.com/sharer/sharer.php?u=" . urlencode( $data['url'] );
-	$linkedin  = "https://www.linkedin.com/sharing/share-offsite/?url=" . urlencode( $data['url'] );
-	$mastodon  = "#";
-	$bluesky   = "https://bsky.app/intent/compose?text=" . urlencode( $data['text'] ) . ' ' . urlencode( $data['url'] );
+	$twitter   = 'https://x.com/intent/tweet?text=' . urlencode( $data['title']  ). '&url=' . urlencode( $data['url'] );
+	$facebook  = 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode( $data['url'] );
+	$linkedin  = 'https://www.linkedin.com/sharing/share-offsite/?url=' . urlencode( $data['url'] );
+	$mastodon  = '#';
+	$bluesky   = 'https://bsky.app/intent/compose?text=' . urlencode( $data['text'] ) . ' ' . urlencode( $data['url'] );
 	
 	return apply_filters(
 		'wpcsp_social_service_links',
@@ -48,7 +48,7 @@ function wpcsp_create_urls( $post_ID ) {
 	);
 }
 
-/*
+/**
  * Generate the HTML links using URLs.
  *
  * @param integer $post_ID of current post
@@ -73,22 +73,22 @@ function wpcsp_create_links( $post_ID ) {
 		if ( $url && $is_enabled ) {
 			$social_icon = wpcsp_social_icon_class( $service );
 			$html       .= "
-					<div class='wpcsp-link $service'>
+					<li class='wpcsp-link " . esc_attr( $service ) . "'>
 						<a href='" . esc_url( $url ) . "' rel='nofollow external' aria-describedby='description-$service'>
-							<span class='wpcsp-icon dashicons dashicons-" . $social_icon . "' aria-hidden='true'></span>
-							<span class='wpcsp-text $service'>" . ucfirst( $service ) . "</span>
+							<span class='wpcsp-icon dashicons dashicons-" . esc_attr( $social_icon ) . "' aria-hidden='true'></span>
+							<span class='wpcsp-text $service'>" . esc_html( ucfirst( $service ) ) . "</span>
 						</a>
-						<span class='description' role='tooltip' id='description-$service'>
+						<span class='description' role='tooltip' id='description-" . esc_attr( $service ) . "'>
 							" . __( 'Share this post' ) . "
 						</span>
-					</div>";
+					</li>";
 		}
 	}
 	
-	return "<div class='wpcsp-links'>" . $html . "</div>";
+	return "<ul class='wpcsp-links'>" . $html . "</ul>";
 }
 
-/*
+/**
  * Fetch HTML for links and wrap in a container. Add heading and ARIA landmark role.
  *
  * @param integer $post_ID of current post.
@@ -107,11 +107,12 @@ function wpcsp_social_block( $post_ID ) {
 	
 	return $html;
 }
-/*
+
+/**
  * Use WordPress filter 'the_content' to add sharing links into post content.
  *
  * @param $content The current content of the post.
- * 
+ *
  * @return $content The previous content of the post plus social sharing links.
  */
 add_filter( 'the_content', 'wpcsp_post_content' );
